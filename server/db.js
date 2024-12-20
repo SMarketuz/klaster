c96ed40c0530fb6d8804a7780bf3bac4f53c7bb9
MOdevco <mirsoonuzbsila001@gmail.com>
2024-12-21 03:11:58 +0500

    1234

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 module.exports = function () {
     // mongoose.connect('mongodb://127.0.0.1:27017/modevco')
-    mongoose.connect('mongodb+srv://modevco:1234alikhan@atlascluster.tirys06.mongodb.net/modevco')
+    mongoose.connect('mongodb+srv://mirsoonuzbsila001:1234modevco@cluster0.oo8yqav.mongodb.net/modevco')
     .then(() => {
         console.log('Mongo ishladi');
     }).catch((err) => {
